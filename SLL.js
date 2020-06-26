a253ccc3561f3cdc409d8af460025caca48f58a0
Alex Kot <makeconstnotvar@protonmail.com>
2020-06-27 00:19:35 +0300

    Singly Linked Lists (SLL)

---
+++ SLL.js
@@ -0,0 +1,132 @@
+class SllNode {
+  constructor(data) {
+    this.data = data;
+    this.nextSibling = null;
+
+  }
+}
+
+class SinglyLinkedList {
+  constructor() {
+    this.head = null;
+  }
+
+  get isEmpty() {
+    return this.head == null;
+  }
+
+  get last() {
+    let current = this.head;
+    while (current.nextSibling != null) {
+      current = current.nextSibling;
+    }
+    return current;
+  }
+
+  insertAtTail(data) {
+    let node = new SllNode(data);
+    if (this.isEmpty)
+      this.head = node;
+    else
+      this.last.nextSibling = node;
+    return this;
+  }
+
+  insertAtHead(data) {
+    let node = new SllNode(data);
+    node.nextSibling = this.head;
+    this.head = node;
+    return this;
+  };
+
+  delete(data) {
+    if (this.head.data === data) {
+      this.head = this.head.nextSibling;
+      return true;
+    }
+    let current = this.head;
+    while (current.nextSibling != null) {
+      if (current.nextSibling.data === data) {
+        current.nextSibling = current.nextSibling.nextSibling;
+        return true
+      } else
+        current = current.nextSibling;
+    }
+    return false;
+  };
+
+  deleteAtTail() {
+    if (this.isEmpty)
+      return false;
+    if (this.head.nextSibling == null) {
+      this.head = null;
+      return true;
+    }
+    let current = this.head;
+    while (current.nextSibling.nextSibling != null) {
+      current = current.nextSibling;
+    }
+    current.nextSibling = null;
+    return true;
+  }
+
+  deleteAtHead() {
+    if (!this.isEmpty)
+      this.head = this.head.nextSibling
+    return this;
+  };
+
+  search(data) {
+    let current = this.head;
+    while (current.data !== data && current.nextSibling != null) {
+      current = current.nextSibling;
+    }
+    return current || null
+  };
+
+  log() {
+    if (this.isEmpty) {
+      console.log("empty")
+    } else {
+      let results = [];
+      let current = this.head;
+      while (current != null) {
+        results.push(current.data);
+        current = current.nextSibling;
+      }
+      console.log(results.join(' -> '))
+    }
+
+  }
+}
+
+module.exports = {SinglyLinkedList, SllNode}
+
+let list = new SinglyLinkedList();
+
+for (let i = 0; i < 10; i++) {
+  list = list.insertAtTail(i);
+}
+
+list.log();
+
+let found = list.search(5)
+
+console.dir(found);
+
+list.deleteAtHead();
+list.deleteAtHead();
+
+list.log();
+
+list.delete(3);
+
+list.log();
+
+list.deleteAtTail();
+list.deleteAtTail();
+list.deleteAtTail();
+list.deleteAtTail();
+list.deleteAtTail();
+
+list.log();
