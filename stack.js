54230216366ea02050c91f8ea936b2bc3e949bde
Aleksand.Kotlyachkov <aleksand.kotlyachkov@x5.ru>
2021-08-10 02:36:14 +0300

    stack

---
+++ stack.js
@@ -0,0 +1,22 @@
+class Stack {
+  constructor() {
+    this.items = [];
+    this.top = null;
+  }
+
+  push(element) {
+  }
+
+  pop() {
+  }
+
+  isEmpty() {
+  }
+
+  getTop() {
+  }
+
+  size() {
+  }
+}
+let stack=new Stack();
