6015dddbe7d1dedf0978fdbd1a97decd62fd49d5
Alex Kot <akotlyachkov@gmail.com>
2021-08-15 00:58:30 +0300

    stack, queue

--- queue.js
+++ queue.js
@@ -2,6 +2,21 @@ class Queue {
   constructor() {
     this.items = [];
   }
+  enqueue(element){
+    this.items.push(element);
+  }
+  dequeue(){
+    return this.items.shift();
+  }
+  isEmpty(){
+    return this.items.length == 0;
+  }
+  getFront(){
+    return this.items[0];
+  }
+  size(){
+    return this.items.length;
+  }
 }
 
 const myQueue = new Queue();
