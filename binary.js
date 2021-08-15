19641591cb545e491dc43431ccf52138d8e5c1eb
Alex Kot <akotlyachkov@gmail.com>
2021-08-15 22:19:16 +0300

    stack, queue

--- binary.js
+++ binary.js
@@ -1,14 +1,13 @@
 const Stack = require("./stack");
 const Queue = require("./queue");
 
-function findBin(n) {
+function findBin1(n) {
   const q = new Stack();
   let bin = '';
   while (n > 0) {
-    q.push(n%2)
+    q.push(n % 2)
     n = Math.floor(n / 2)
   }
-  console.log(q.data.join(' | '))
   while (!q.empty) {
     bin += q.pop();
   }
@@ -16,4 +15,25 @@ function findBin(n) {
 }
 
 
-  console.log(findBin(4))
+console.log(findBin1(10))
+
+
+function findBin2(n) {
+  let result = [];
+  let q = new Queue();
+  let s1, s2;
+  q.push("1");
+  for (let i = 0; i < n; i++) {
+    let x = q.pop()
+    result.push(x);
+    s1 = result[i] + "0";
+    s2 = result[i] + "1";
+
+    q.push(s1);
+    q.push(s2);
+  }
+
+  return result;
+}
+
+console.log(findBin2(10))
