83fe3dff8f5d0d01f1499b64cf255eb825adfb72
Alex Kot <akotlyachkov@gmail.com>
2021-08-20 02:12:02 +0300

    stack sort

---
+++ sort.js
@@ -0,0 +1,61 @@
+const Stack = require("./stack");
+
+function sort1(stack) {
+  const temp = new Stack();
+  let val;
+  while (!stack.empty) {
+    val = stack.pop();
+    if (val <= temp.last) {
+      temp.push(val);
+    } else {
+      while (!temp.empty) {
+        stack.push(temp.pop());
+      }
+      temp.push(val);
+    }
+  }
+  while (!temp.empty) {
+    stack.push(temp.pop());
+  }
+  return stack;
+}
+
+function sort2(stack) {
+  if (!stack.empty) {
+    const val = stack.pop();
+    sort2(stack);
+    insert(stack, val);
+  }
+
+  function insert(stack, val) {
+    if (stack.empty || val < stack.last) {
+      stack.push(val);
+    } else {
+      const temp = stack.pop();
+      insert(stack, val);
+      stack.push(temp);
+    }
+  }
+
+  return stack
+}
+
+function getRandomArbitrary(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+const len = 50;
+const items = new Array(len)
+for (let i = 0; i < len; i++) {
+  items[i] = getRandomArbitrary(0, 100);
+}
+//const stack = new Stack([1, 3, 2]);
+//const stack = new Stack([1, 3, 2, 5, 5, 16, 4, 18, 19, 19]);
+const stack = new Stack(items);
+
+let start = process.hrtime();
+const sorted = sort2(stack);
+let stop = process.hrtime(start)
+console.log(`Время: ${(stop[0] * 1e9 + stop[1]) / 1e9} сек`)
+console.log(sorted.data);
