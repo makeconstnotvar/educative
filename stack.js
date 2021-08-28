86812fb5159662f36b9ece619d7df30456d80dbb
Alex Kot <akotlyachkov@gmail.com>
2021-08-29 00:56:32 +0300

    min stack

--- stack.js
+++ stack.js
@@ -1,20 +1,37 @@
 class Stack {
-  constructor(items = []) {
+  constructor(items = [], initMin = true) {
     this.items = items.reverse();
+    if (initMin) {
+      const m = Math.min(...this.items);
+      this.minimum = new Stack(this.items.map(x => m), false);
+    }
     let len = items.length;
     this.last = len ? this.items[len - 1] : null;
   }
 
-  push(element) {
-    this.items.push(element);
-    this.last = element;
+  push(value) {
+    if (this.minimum) {
+      if (value <= this.minimum.last || this.minimum.empty) {
+        this.minimum.push(value)
+      } else {
+        this.minimum.push(this.minimum.last)
+      }
+    }
+
+    this.items.push(value);
+    this.last = value;
   }
 
   pop() {
+    this.minimum.pop();
     const last = this.items.pop();
     const len = this.items.length;
     this.last = (!last || len == 0) ? null : this.items[len - 1]
     return last;
+  }
+
+  get min() {
+    return this.minimum.last;
   }
 
   get empty() {
@@ -34,7 +51,8 @@ class Stack {
   }
 
   log() {
-    console.log(this.items);
+    console.log('main', this.items);
+    console.log('mins', this.minimum.items);
   }
 }
 
--- test.js
+++ test.js
@@ -1,91 +1,27 @@
-class Stack {
-  constructor(items) {
-    this.items = items || [];
-    this.top = null;
-  }
+const Stack = require('./stack');
 
-  getTop() {
-    if (this.items.length == 0)
-      return null;
-    return this.top;
-  }
-
-  isEmpty() {
-    return this.items.length == 0;
-  }
-
-  size() {
-    return this.items.length;
-  }
-
-  push(element) {
-    this.items.push(element);
-    this.top = element;
-  }
-
-  pop() {
-    if (this.items.length != 0) {
-      if (this.items.length == 1) {
-        this.top = null;
-        return this.items.pop();
-      } else {
-        this.top = this.items[this.items.length - 2];
-        return this.items.pop();
-      }
-
-    } else
-      return null;
-  }
-log(){
-    console.log(this.items)
-}
-
-}
-function sortStack(stack) {
-  //1. Use a second tempStack.
-  var tempStack = new Stack();
-  var value;
-  while (stack.isEmpty() == false) {
-    //2. Pop value from mainStack.
-    value = stack.pop();
-    //3. If the value is greater than or equal to the top of tempStack, then push the value in tempStack
-    if (value >= tempStack.getTop()) {
-      tempStack.push(value)
-    } else {
-      // else pop all values from tempStack and push them in mainStack
-      while (tempStack.isEmpty() == false) {
-        stack.push(tempStack.pop());
-      }
-      // and in the end push value in tempStack
-      tempStack.push(value);
-    }
-    //4.repeat from step 2 till mainStack is not empty.
-  }
-//5. When mainStack will be empty, tempStack will have sorted values in descending order.
-//6. Now transfer values from tempStack to mainStack to make values sorted in ascending order.
-  while (tempStack.isEmpty() == false) {
-    stack.push(tempStack.pop());
-
-  }
-  return stack;
-
-}
 function getRandomArbitrary(min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
-const len = 35;
+
+const len = 20;
 const items = new Array(len)
 for (let i = 0; i < len; i++) {
   items[i] = getRandomArbitrary(0, 100);
 }
-var stack =new Stack(items)
 
+const stack = new Stack(items)
+stack.push(3)
+stack.push(2)
+stack.push(22)
+stack.push(10)
+stack.push(1)
+stack.push(10)
 let start = process.hrtime();
-
-sortStack(stack)
+console.log(stack.min);
 let stop = process.hrtime(start)
 console.log(`Время: ${(stop[0] * 1e9 + stop[1]) / 1e9} сек`)
 
- stack.log()
+stack.log()
