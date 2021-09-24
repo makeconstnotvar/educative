c90ec582228bd86d649f355a0ce924eaff323124
Alex Kot <akotlyachkov@gmail.com>
2021-09-24 23:47:33 +0300

    BFS

--- graph.js
+++ graph.js
@@ -1,7 +1,8 @@
 const {SinglyLinkedList} = require("./sll");
+const Queue = require("./queue");
 
 class Graph {
-  constructor(vertices = []) {
+  constructor(vertices = 1) {
     //Total number of vertices in the graph
     this.vertices = vertices;
     //Defining an array which can hold LinkedLists equal to the number of vertices in the graph
@@ -9,13 +10,13 @@ class Graph {
     //Creating a new LinkedList for each vertex/index of the list
     for (let i = 0; i < vertices; i++) {
       let temp = new SinglyLinkedList();
-      this.list.push(temp);
+      this.list[i] = temp;
     }
   }
 
   addEdge(source, destination) {
-    if (source < this.vertices && destination < this.vertices)
-      this.list[source].insertAtHead(destination);
+    if (source <= this.vertices && destination <= this.vertices)
+      this.list[source].insertAtTail(destination);
     //ненаправленный this.list[destination].insertAtHead(source)
   }
 
@@ -28,14 +29,58 @@ class Graph {
         log += "[" + String(temp.data) + "] > ";
         temp = temp.next;
       }
-      log+='null\n'
+      log += 'null\n'
 
     }
     console.log(log);
   }
+
+  bfsTraversal_helper(source, visited, obj) {
+
+    let queue = new Queue();
+    queue.push(source);
+    visited[source] = true;
+
+    while (!queue.empty) {
+
+      let current_node = queue.shift();
+      obj.result += current_node;
+      let temp = this.list[current_node].getHead();
+      while (temp != null) {
+        if (visited[temp.data] == false) {
+          queue.shift(temp.data);
+          visited[temp.data] = true;
+        }
+        temp = temp.next;
+      }
+    }
+  }
+
+  bfsTraversal() {
+    if (this.vertices < 1) {
+      return null;
+    }
+
+    let obj = {result: ''}
+
+    let visited = [];
+    for (let x = 0; x < this.vertices; x++) {
+      visited.push(false);
+    }
+
+    for (let i = 0; i < this.vertices; i++) {
+      if (!visited[i])
+        this.bfsTraversal_helper(i, visited, obj);
+    }
+
+    return obj.result;
+  }
 }
 
-const g = new Graph(3);
-g.addEdge(0, 1)
-g.addEdge(0, 2)
+const g = new Graph(5);
+g.addEdge(1, 2)
+g.addEdge(1, 3)
+g.addEdge(2, 4)
+g.addEdge(2, 5)
 g.printGraph();
+console.log(g.bfsTraversal());
